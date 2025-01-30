9091070c1f9691b3d4693eb4eda4f912d177dac4
Tafshi Uthshow Hoque <69767127+Draxgter1001@users.noreply.github.com>
2025-01-30 14:59:00 +0000

    Update PlantController.java
    Frontend URL

--- src/main/java/com/example/taf/VPI/controller/PlantController.java
+++ src/main/java/com/example/taf/VPI/controller/PlantController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://venomous-plant-identifier-frontend.vercel.app/")
 @RestController
 @RequestMapping("/api/plants")
 public class PlantController {
