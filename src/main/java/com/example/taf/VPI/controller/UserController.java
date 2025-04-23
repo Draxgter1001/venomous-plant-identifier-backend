761287e3aa474cc2b125b91e356899701c2f0ceb
Tafshi Uthshow Hoque <69767127+Draxgter1001@users.noreply.github.com>
2025-04-23 15:02:18 +0100

    Update UserController.java
    Added email/password validations + password recovery

--- src/main/java/com/example/taf/VPI/controller/UserController.java
+++ src/main/java/com/example/taf/VPI/controller/UserController.java
@@ -4,11 +4,11 @@ import com.example.taf.VPI.model.User;
 import com.example.taf.VPI.repository.UserRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
+@CrossOrigin(origins = {"http://localhost:3000", "https://venomous-plant-identifier-frontend.vercel.app"}, allowCredentials = "true")
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
@@ -25,6 +25,14 @@ public class UserController {
         if(userRepository.findByEmail(user.getEmail()) != null){
             return ResponseEntity.badRequest().body("Email Already Exists");
         }
+
+        if(user.getEmail().isEmpty() || !user.getEmail().matches("^[^@\\s]+@[^@\\s]+\\.[^@\\s]+$")){
+            return ResponseEntity.badRequest().body("Invalid Email Format");
+        }
+
+        if(user.getPassword().isEmpty() || user.getPassword().length() < 8){
+            return ResponseEntity.badRequest().body("Password needs to be at least 8 characters");
+        }
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
         return ResponseEntity.ok().body("User Registered");
@@ -37,5 +45,25 @@ public class UserController {
             return ResponseEntity.badRequest().body("Incorrect email or password");
         }
         return ResponseEntity.ok().body("User Logged In");
+    }
+
+    @PostMapping("/reset-password")
+    public ResponseEntity<String> resetPassword(@RequestBody Map<String, String> payload){
+        String email = payload.get("email");
+        String newPassword = payload.get("newPassword");
+
+        if(email == null || newPassword == null || newPassword.length() < 8 ){
+            return ResponseEntity.badRequest().body("Invalid email or password");
+        }
+
+        User user = userRepository.findByEmail(email);
+        if(user == null){
+            return ResponseEntity.badRequest().body("User Not Found");
+        }
+
+        user.setPassword(passwordEncoder.encode(newPassword));
+        userRepository.save(user);
+
+        return ResponseEntity.ok().body("Password Reset Successful");
     }
 }
